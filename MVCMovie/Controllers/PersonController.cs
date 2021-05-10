b9dce6dc2bf23edcac3268483a51bab8dd6e3b91
htorrico <htorrico@tecsup.edu.pe>
2021-05-10 17:47:05 -0500

    Agregar archivos de proyecto.

---
+++ MVCMovie/Controllers/PersonController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        #region Views
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+
+            ViewBag.Title = "List of People";
+            ViewBag.Message = "Please, don't slepp!";
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        #endregion
+
+        public string GetFullName(string firstname, string lastname)
+        {
+            return string.Concat(firstname, " ", lastname);
+        }
+
+
+    }
+}
